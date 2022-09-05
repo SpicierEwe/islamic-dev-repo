ccaca8230aad66a0c484a44d4e1d4d4d7376b721
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2022-09-05 12:19:44 -0700

    aa

--- .history/pages/[...id]/index_20220905121540.js
+++ .history/pages/[...id]/index_20220905121540.js
@@ -25,7 +25,7 @@ export async function getStaticPaths() {
   const hostName =
     process.env.NODE_ENV != "production"
       ? "http://localhost:3000"
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+      : `https://${NEXT_PUBLIC_VERCEL_URL}`;
 
   const res = await fetch(`${hostName}/api/generate_static_paths`);
 
@@ -44,7 +44,7 @@ export async function getStaticProps({ params }) {
   const hostName =
     process.env.NODE_ENV != "production"
       ? "http://localhost:3000"
-      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+      : `https://${NEXT_PUBLIC_VERCEL_URL}`;
 
   // this api watches all routes its will see the 2 query prams
   // if they match with the data in database then that data will be sent back
