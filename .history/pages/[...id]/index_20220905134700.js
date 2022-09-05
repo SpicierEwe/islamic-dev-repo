14a33c7d36254edbf9aaebfd9367632acfa45fae
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2022-09-05 13:53:10 -0700

    123

---
+++ .history/pages/[...id]/index_20220905134700.js
@@ -0,0 +1,51 @@
+import React, { Fragment, useContext, useEffect } from "react";
+import DisplayDownloadable from "../../components/display_resources/display_downloadable";
+import DownloadableContext from "../../store/doenloadable_context";
+
+import styles from "../../styles/Home.module.css";
+
+export default function DynamicPage(props) {
+  // getting hold of the context
+  const DownloadableCtx = useContext(DownloadableContext);
+
+  useEffect(() => {
+    // globalising data here
+    DownloadableCtx.globaliseDataFunction(props.responseData), [];
+  });
+
+  return (
+    <div>
+      {/* displays downloadable explore the component to know more */}
+      <DisplayDownloadable></DisplayDownloadable>
+    </div>
+  );
+}
+
+export async function getStaticPaths(req) {
+  // fetching dynamically generated paths
+  const res = await fetch(`${req.protocol}/api/generate_static_paths`);
+
+  const x = await res.json();
+  // console.log(x["paths"]);
+
+  return {
+    paths: x["paths"],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const [query1, query2] = params.id;
+
+  console.log(prams.protocol);
+
+  // this api watches all routes its will see the 2 query prams
+  // if they match with the data in database then that data will be sent back
+  const res = await fetch(`${process.env.host_name}/api/${query1}/${query2}`);
+  const responseData = await res.json();
+  return {
+    props: {
+      responseData,
+    },
+  };
+}
