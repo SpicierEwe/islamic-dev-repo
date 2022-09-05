87be2acf0c367a10ebbdce43ebc9ea3625c07dc9
SpicierEwe <83544142+SpicierEwe@users.noreply.github.com>
2022-09-05 13:21:01 -0700

    aa

---
+++ .history/next.config_20220905132056.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "upload.wikimedia.org",
+      "previews.123rf.com",
+      "img1.pnghut.com",
+      "www.pngfind.com",
+      "www.kindpng.com",
+      "toppng.com",
+      "cdn-icons-png.flaticon.com",
+      "i0.wp.com",
+      "en.quran.com.kw",
+      "download1585.mediafire.com",
+      "cdn.icon-icons.com",
+    ],
+  },
+
+  env: {
+    host_name:
+      process.env.NODE_ENV == "development"
+        ? "http://localhost:3000"
+        : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+  },
+};
+
+module.exports = nextConfig;
